6f5506ee2410702813719cd3830ef9a1d904dae6
GrishinAnton <asegrishin@sberbank.ru>
2020-05-17 07:23:18 +0300

    Создание модели. Добавление библиотеки компонентов

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
+//style
+import './app.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
