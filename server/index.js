e5177f62ba3ba73c504fb513da3866eeaa6815dd
GrishinAnton <asegrishin@sberbank.ru>
2020-05-17 08:32:55 +0300

    Настройка связки с сервером

--- server/index.js
+++ server/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World'));
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/singup', (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
+});
 
 app.listen(port, () => console.log(`Start:${port}`));
