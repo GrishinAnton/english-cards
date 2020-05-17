e14bf9ee49697dd25831ecfa8a0fa6da6208326e
GrishinAnton <asegrishin@sberbank.ru>
2020-05-17 17:33:34 +0300

    Настроил роутер

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,23 @@
-import React, { useState } from 'react';
-import { Row, Col, Tabs, Tab, Container } from 'react-bootstrap';
-
-import SingIn from './components/SingIn'
-import SingUp from './components/SingUp'
-
+//@ts-ignore
+import React from "react";
+import { Router, Switch, Route } from "react-router";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import history from "./utils/browserHistory";
 
 const App = () => {
-
-  const [tabs, setTabs] = useState<string>('singin')
-
   return (
-    <Container>
-      <Row>
-        <Col md={4}>
-          <Tabs
-            id="controlled-tab-example"
-            activeKey={tabs}
-            onSelect={(k: string) => setTabs(k)}
-          >
-            <Tab eventKey="singin" title="Войти">
-              <SingIn></SingIn>
-            </Tab>
-            <Tab eventKey="singup" title="Зарегестрироваться">
-              <SingUp></SingUp>
-            </Tab>
-
-          </Tabs>
-        </Col>
-      </Row>
-    </Container>
+    <Router history={history}>
+      <Switch>
+        <Route path="/" exact>
+          <Login />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/Login/index.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Row, Col, Tabs, Tab, Container } from "react-bootstrap";
+
+import SingIn from "../SingIn";
+import SingUp from "../SingUp";
+
+const Login = () => {
+  const [tabs, setTabs] = useState<string>("singin");
+
+  return (
+    <Container>
+      <Row>
+        <Col md={4}>
+          <Tabs
+            id="controlled-tab-example"
+            activeKey={tabs}
+            onSelect={(k: string) => setTabs(k)}
+          >
+            <Tab eventKey="singin" title="Войти">
+              <SingIn></SingIn>
+            </Tab>
+            <Tab eventKey="singup" title="Зарегестрироваться">
+              <SingUp></SingUp>
+            </Tab>
+          </Tabs>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Login;
