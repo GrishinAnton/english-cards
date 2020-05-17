dbe0ddfff6388d20d2659b6ca45969fe164b6a08
GrishinAnton <asegrishin@sberbank.ru>
2020-05-17 07:23:18 +0300

    Создание модели. Добавление библиотеки компонентов

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <header className="App-header">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="primary">Primary</Button>
       </header>
     </div>
   );
---
+++ server/models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose")
+
+const SingInSchema = new.mongoose.Schema({
+    email: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    password: {
+        type: Number,
+        trim: true,
+        required: true
+    }
+})
+
+const SingUpSchema = new.mongoose.Schema({
+    email: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    password: {
+        type: Number,
+        trim: true,
+        required: true
+    }
+})
+
+const ProfileSchema = new.mongoose.Schema({
+    name: {
+        type: String
+    },
+    sername: {
+        type: String
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    sex: {
+        type: String
+    },
+    avatar: {
+        type: String
+    }
+
+})
+
+module.exports = mongoose.model("SingIn", SingInSchema)
+module.exports = mongoose.model("SingUp", SingUpSchema)
+module.exports = mongoose.model("ProfileSchema", ProfileSchema)
