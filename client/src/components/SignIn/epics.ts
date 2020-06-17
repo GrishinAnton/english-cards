09991fdc6409cf24a810d18bb00cda8fd806894f
Михаил Чеберя <cheberya.m.a@sberbank.ru>
2020-06-18 02:51:31 +0300

    добавил сайд-эффекты в эпик и подключил всё к вьюхе

--- client/src/components/SignIn/epics.ts
+++ client/src/components/SignIn/epics.ts
@@ -1,16 +1,21 @@
 import { Epic } from 'redux-observable';
 import { from, of } from 'rxjs';
-import { filter, switchMap, map, catchError, mergeMap, exhaustMap } from 'rxjs/operators';
+import { filter, switchMap, map, catchError, tap } from 'rxjs/operators';
 import { RootAction, RootState, Services, isActionOf } from 'typesafe-actions';
 
+import history from '../../utils/browserHistory'
 import { updateSignInAsync } from './actions';
 
 const signInEpic: Epic<RootAction, RootAction, RootState, Services> = (action$, state$, { api }) =>
   action$.pipe(
     filter(isActionOf(updateSignInAsync.request)),
-    exhaustMap(action =>
+    switchMap(action =>
       from(api.signIn.update(action.payload)).pipe(
         map(updateSignInAsync.success),
+        tap(action => {
+          localStorage.setItem('token', action.payload.token);
+          history.push('/dashboard');
+        }),
         catchError(error => of(updateSignInAsync.failure(error))),
       )
     ),
