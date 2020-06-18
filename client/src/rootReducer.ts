97fc846299bc9b3fc45311e6149d62c402079e66
Михаил Чеберя <cheberya.m.a@sberbank.ru>
2020-06-18 03:07:44 +0300

    удалил легаси код и пакеты

--- client/src/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit'
-import { userReducer } from './components/SignIn/ducks'
-
-const rootReducers = combineReducers({
-	userReducer,
-})
-
-export default rootReducers
