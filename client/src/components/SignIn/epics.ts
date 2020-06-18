7e78d63b46551aeb15f203be4640acd4675a1f13
Михаил Чеберя <cheberya.m.a@sberbank.ru>
2020-06-18 03:03:12 +0300

    уведомление при успешном входе и при ошибке (отдельный эпик под ошибку)

--- client/src/components/SignIn/epics.ts
+++ client/src/components/SignIn/epics.ts
@@ -1,9 +1,10 @@
 import { Epic } from 'redux-observable';
 import { from, of } from 'rxjs';
-import { filter, switchMap, map, catchError, tap } from 'rxjs/operators';
+import { filter, switchMap, map, catchError, tap, ignoreElements } from 'rxjs/operators';
 import { RootAction, RootState, Services, isActionOf } from 'typesafe-actions';
 
 import history from '../../utils/browserHistory'
+import { Notification } from '../../utils'
 import { updateSignInAsync } from './actions';
 
 const signInEpic: Epic<RootAction, RootAction, RootState, Services> = (action$, state$, { api }) =>
@@ -15,10 +16,18 @@ const signInEpic: Epic<RootAction, RootAction, RootState, Services> = (action$, 
         tap(action => {
           localStorage.setItem('token', action.payload.token);
           history.push('/dashboard');
+          Notification('Рады видеть вас!');
         }),
         catchError(error => of(updateSignInAsync.failure(error))),
       )
     ),
   );
 
-export default [signInEpic];
+const signInErrorEpic: Epic<RootAction, RootAction, RootState, Services> = (action$) =>
+  action$.pipe(
+    filter(isActionOf(updateSignInAsync.failure)),
+    tap(() => Notification('Извините, что-то пошло не так:(')),
+    ignoreElements(),
+  );
+
+export default [signInEpic, signInErrorEpic];
